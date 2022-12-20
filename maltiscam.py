43df3b1a16387e0c2e0737a2cb00cdde3daa6a39
nico <suttonnico2@gmail.com>
2022-12-20 08:56:47 -0300

    test

--- maltiscam.py
+++ maltiscam.py
@@ -1,9 +1,22 @@
 import cv2
 import time
 
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
 cam = 0
 camera_0 = cv2.VideoCapture(cam)
 
 time.sleep(2)
 s0, img = camera_0.read()
+img = increase_brightness(img)
 cv2.imwrite('maltis.png', img)
