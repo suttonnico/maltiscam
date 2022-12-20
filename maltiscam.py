88cf887e160437dca22e859c143d1143142f4dac
nico <suttonnico2@gmail.com>
2022-12-20 09:26:30 -0300

    test

--- maltiscam.py
+++ maltiscam.py
@@ -1,5 +1,20 @@
 import cv2
 import time
+from git import Repo
+
+PATH_OF_GIT_REPO = r'.'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'maltis'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+
 
 
 
@@ -9,3 +24,4 @@ camera_0 = cv2.VideoCapture(cam)
 time.sleep(2)
 s0, img = camera_0.read()
 cv2.imwrite('maltis.png', img)
+git_push()
