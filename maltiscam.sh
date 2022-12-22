9c921adfc544500c26d7c3ed001ecac08881250f
nico <suttonnico2@gmail.com>
2022-12-22 14:06:02 -0300

    test

--- maltiscam.sh
+++ maltiscam.sh
@@ -2,3 +2,5 @@ eval "$(ssh-agent -s)"
 printf "malta\n" | ssh-add /home/pi/Documents/maltis/maltiscam/key
 ssh -vT git@github.com
 python /home/pi/Documents/maltis/maltiscam/maltiscam.py
+cd /home/pi/Documents/maltis/maltiscam/
+git push
